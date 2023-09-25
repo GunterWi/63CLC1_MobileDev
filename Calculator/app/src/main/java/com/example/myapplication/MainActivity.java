6221c6ebe2b4e6424f809e5d0eb58b8fde07c713
GunterWi <boi09456@gmail.com>
2023-09-25 20:09:10 +0700

    upload

--- Calculator/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Calculator/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.InputMismatchException;
@@ -16,36 +17,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public int Input(TextView in){
-        int num = 0;
+    public float Input(TextView in){
+        float num = 0f;
         try {
-            num = Integer.parseInt(in.getText().toString());
+            num = Float.parseFloat(in.getText().toString());
         } catch (NumberFormatException e) {
-            Log.d("info", "Nhập sai");
+            Log.d("info", "Wrong input");
         }
         return num;
     }
 
-    public void CalculateClick(View v){
-        int h = Input(findViewById(R.id.HeightTb));
-        int w = Input(findViewById(R.id.WeightTb));
-        double result = (w * 1.0) / Math.pow(h / 100.0, 2);
-        Log.d("info",Double.toString(Math.pow(h / 100.0, 2)));
-        TextView mTextView = findViewById(R.id.Point);
-        mTextView.setText(String.valueOf(String.format("%.1f", result))); // Chuyển đổi result thành chuỗi và đặt vào TextView
-        TextView StatusView = findViewById(R.id.Status);
-        if(result<18.5){
-            StatusView.setText("Underweight");
-        }else if(result<25){
-            StatusView.setText("Normal");
-        }else if(result<30){
-            StatusView.setText("Overweight");
-        }else if(result<35){
-            StatusView.setText("Obese Class I");
-        }else if(result<40){
-            StatusView.setText("Obese Class II");
-        }else{
-            StatusView.setText("Obese Class III");
+    public void Action(View v) {
+        float l = Input(findViewById(R.id.num1));
+        float r = Input(findViewById(R.id.num2));
+        Button button = (Button)v;
+        TextView mTextView = findViewById(R.id.result);
+
+        String check = button.getText().toString();
+        double result = 0.0;
+        if (check.equals("+")) {
+            result = l + r;
+        } else if (check.equals("-")) {
+            result = l - r;
+        } else if (check.equals("*")) {
+            result = l * r;
+        } else if (check.equals("/")) {
+            if (r != 0) {
+                result = l / r;
+            } else {
+                Log.d("info", "Không thể chia cho 0");
+                return;
+            }
+        } else {
+            Log.d("info", "Chưa chọn nút");
+            return;
         }
+
+        String formattedResult = String.format("%.1f", result);
+        mTextView.setText(l + " " + check + " " + r + " = " + formattedResult);
     }
+
 }
