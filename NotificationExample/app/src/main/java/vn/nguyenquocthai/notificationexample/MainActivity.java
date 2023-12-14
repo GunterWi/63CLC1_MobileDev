2f3993a1c783b50842004fd4730a4eb56c5790b8
GunterWi <boi09456@gmail.com>
2023-12-14 20:51:35 +0700

    Upload

---
+++ NotificationExample/app/src/main/java/vn/nguyenquocthai/notificationexample/MainActivity.java
@@ -0,0 +1,18 @@
+package vn.nguyenquocthai.notificationexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+
+        }
+    }
+}
