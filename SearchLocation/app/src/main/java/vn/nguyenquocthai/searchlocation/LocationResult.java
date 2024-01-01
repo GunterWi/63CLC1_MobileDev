33fe2d25f23c21f99f0b70e6bf755b25470f0659
GunterWi <boi09456@gmail.com>
2024-01-01 23:07:01 +0700

    Upload

--- SearchLocation/app/src/main/java/vn/nguyenquocthai/searchlocation/LocationResult.java
+++ SearchLocation/app/src/main/java/vn/nguyenquocthai/searchlocation/LocationResult.java
@@ -2,9 +2,13 @@ package vn.nguyenquocthai.searchlocation;
 
 public class LocationResult {
     private String display_name;
+    private double lat;
+    private double lon;
 
-    public LocationResult(String display_name) {
+    public LocationResult(String display_name, double lat, double lon) {
         this.display_name = display_name;
+        this.lat = lat;
+        this.lon = lon;
     }
 
     public String getDisplay_name() {
@@ -13,5 +17,21 @@ public class LocationResult {
 
     public void setDisplay_name(String display_name) {
         this.display_name = display_name;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
     }
 }
--- SearchLocation/app/src/main/java/vn/nguyenquocthai/searchlocation/MainActivity.java
+++ SearchLocation/app/src/main/java/vn/nguyenquocthai/searchlocation/MainActivity.java
@@ -7,14 +7,17 @@ import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.android.volley.Response;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import retrofit2.Call;
@@ -29,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private ArrayAdapter<String> adapter;
 
-    private Handler searchHandler = new Handler(); // giữ cho 0.5s hoặc 0.2s mới thực hiện tìm kiếm
-    private Runnable searchRunnable;
+    private List<String> cities = new ArrayList<>(Arrays.asList("Hà Nội", "Thành phố Hồ Chí Minh", "Đà Nẵng", "Hải Phòng", "Cần Thơ", "Nha Trang", "Hải Dương", "..."));
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,54 +47,78 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         openStreetMapApi = retrofit.create(OpenStreetMapApi.class);
 
+
         searchView = findViewById(R.id.search_view);
         listView = findViewById(R.id.list_view);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        listView.setVisibility(View.GONE);
+        searchView.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                listView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listView.setVisibility(View.VISIBLE);
+            }
+        });
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities);
         listView.setAdapter(adapter);
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                searchHandler.removeCallbacks(searchRunnable);
                 searchLocations(query);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                searchHandler.removeCallbacks(searchRunnable);
-                searchRunnable = () -> searchLocations(newText);
-                searchHandler.postDelayed(searchRunnable, 200);
+                // Lọc danh sách dựa trên newText
+                adapter.getFilter().filter(newText);
                 return false;
+            }
+        });
+
+        // Xử lý sự kiện chọn một gợi ý
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedCity = adapter.getItem(position);
+            if (selectedCity != null) {
+                searchLocations(selectedCity);
+                //Toast.makeText(this,selectedCity,Toast.LENGTH_LONG).show();
             }
         });
     }
 //https://nominatim.openstreetmap.org/search?format=json&q=nha%20trang
-    private void searchLocations(String query) {
-        Call<List<LocationResult>> call = openStreetMapApi.searchLocations("json", query);
-        call.enqueue(new Callback<List<LocationResult>>() {
+private void searchLocations(String query) {
+    Call<List<LocationResult>> call = openStreetMapApi.searchLocations("json", query);
+    call.enqueue(new Callback<List<LocationResult>>() {
 
-            @Override
-            public void onResponse(Call<List<LocationResult>> call, retrofit2.Response<List<LocationResult>> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    List<String> locationNames = new ArrayList<>();
-                    for (LocationResult location : response.body()) {
-                        locationNames.add(location.getDisplay_name());
-                    }
-
-                    // Cập nhật dữ liệu của adapter trên UI thread
-                    runOnUiThread(() -> {
-                        adapter.clear();
-                        adapter.addAll(locationNames);
-                        adapter.notifyDataSetChanged();
-                    });
+        @Override
+        public void onResponse(Call<List<LocationResult>> call, retrofit2.Response<List<LocationResult>> response) {
+            if (response.isSuccessful() && response.body() != null) {
+                List<String> locationDetails = new ArrayList<>();
+                for (LocationResult location : response.body()) {
+                    String detail = location.getDisplay_name() + "\nLat: " + location.getLat() + ", Lon: " + location.getLon();
+                    locationDetails.add(detail);
+                    Toast.makeText(MainActivity.this,detail,Toast.LENGTH_LONG).show();
                 }
-            }
 
-            @Override
-            public void onFailure(Call<List<LocationResult>> call, Throwable t) {
-                // Xử lý khi có lỗi xảy ra
+                // Cập nhật dữ liệu của adapter trên UI thread
+                runOnUiThread(() -> {
+                    adapter.clear();
+                    adapter.addAll(locationDetails);
+                    adapter.notifyDataSetChanged();
+                });
             }
-        });
-    }
+        }
+
+        @Override
+        public void onFailure(Call<List<LocationResult>> call, Throwable t) {
+            // Xử lý khi có lỗi xảy ra
+        }
+    });
+}
+
 }
